4ae4daba1e1ee9aef709e3e3f20d7d229616cc55
mirinae-i <99980609+mirinae-i@users.noreply.github.com>
2023-09-19 00:07:17 +0900

    First Release

---
+++ src/main/java/com/mirinae/sekai_force/util/JasyptConfig.java
@@ -0,0 +1,33 @@
+package com.mirinae.sekai_force.util;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Bean(name = "jasyptEncryptor")
+    public StringEncryptor stringEncryptor() {
+        final String KEY = "";
+        final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        final SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(KEY);
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
